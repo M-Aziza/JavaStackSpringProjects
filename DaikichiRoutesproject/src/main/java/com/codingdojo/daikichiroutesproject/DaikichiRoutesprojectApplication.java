e2daf1868f2dec40e7b93737bfb761f1893f0050
Aziza Alshahrani <105348844+M-Aziza@users.noreply.github.com>
2022-07-24 15:27:26 +0300

    Add files via upload

---
+++ DaikichiRoutesproject/src/main/java/com/codingdojo/daikichiroutesproject/DaikichiRoutesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichiroutesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesprojectApplication.class, args);
+	}
+
+}
---
+++ DaikichiRoutesproject/src/test/java/com/codingdojo/daikichiroutesproject/DaikichiRoutesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.daikichiroutesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
